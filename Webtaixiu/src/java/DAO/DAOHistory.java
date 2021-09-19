ae75dc8da503e136992c60983b4e409ec4d9fc4d
longvo1w1 <62983165+longvo1w1@users.noreply.github.com>
2021-09-19 21:48:32 +0700

    update

--- Webtaixiu/src/java/DAO/DAOHistory.java
+++ Webtaixiu/src/java/DAO/DAOHistory.java
@@ -5,10 +5,32 @@
  */
 package DAO;
 
+import static DAO.DAOHistoryRoom.conn;
+import static DAO.DAOHistoryRoom.temp;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author Admin
  */
 public class DAOHistory {
-    
+
+    public static void InsertHistory(String user_name, String betresult, String bet, int betpoint, int betwin) {
+        try {
+            conn = temp.connect();
+            PreparedStatement stmt = conn.prepareStatement("Insert into history_room values(?, ?, ?, ?, ?)");
+            stmt.setInt(1, 0);
+            stmt.setString(2, user_name);
+            stmt.setString(3, betresult);
+            stmt.setString(4, bet);
+            stmt.setInt(5, betpoint);
+            stmt.setInt(6, betwin);
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
